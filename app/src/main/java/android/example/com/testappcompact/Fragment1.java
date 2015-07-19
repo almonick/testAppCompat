50285b65199f66ff0a4907e5fc5fa7c4e854933b
almonick <almonick72@gmail.com>
2015-07-19 23:40:50 +0300

    init

---
+++ app/src/main/java/android/example/com/testappcompact/Fragment1.java
@@ -0,0 +1,24 @@
+package android.example.com.testappcompact;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class Fragment1 extends Fragment {
+
+    public Fragment1() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
---
+++ app/src/main/java/android/example/com/testappcompact/MainActivity.java
@@ -0,0 +1,70 @@
+package android.example.com.testappcompact;
+
+import android.support.v7.app.ActionBar;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setupToolbar();
+        getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, new Fragment1()).commit();
+    }
+
+    private void setupToolbar(){
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        if(mToolbar != null) {
+            setSupportActionBar(mToolbar);
+        }
+
+        final ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null) {
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_drawer);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch (item.getItemId()){
+            case R.id.action_search:
+
+                break;
+            case R.id.action_refresh:
+                onReferesh();
+                break;
+        }
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void onReferesh() {
+        Toast.makeText(this, "refresh pressed", Toast.LENGTH_SHORT).show();
+    }
+}
